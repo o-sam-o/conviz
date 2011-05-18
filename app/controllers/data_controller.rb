5bac5a6c32ae0bedbf78d3cd78e9d43be8dd2641
Sam Cavenagh <cavenaghweb@hotmail.com>
2011-05-18 21:55:01 +1000

    move sql queries out of controller into model

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,46 +1,25 @@
 class DataController < ApplicationController
 
   def boats
-    @boats = query_to_map("select boat, count(*) as convicts, count(distinct(departure_date)) as voyage_count from convicts where boat is not null group by boat order by boat")
-    @top_boats = query_to_map("select boat, count(*) as convicts from convicts group by boat order by count(*) desc limit 100")
+    @boats = Convict.boat_stats
+    @top_boats = Convict.top_boat_stats
   end
 
   def destinations
-    @destinations = query_to_map("select destination, destinations.state, count(*) as convicts from convicts left join destinations on destinations.name = convicts.destination where destination  is not null group by destination order by destination")
-    @aust_destinations = @destinations.inject({}) do |result, value|
-      destination = Destination.find_by_name(value[:destination])
-      result["#{destination.current_name}"] = value[:convicts] unless destination.blank?
-      result
-    end
-    @state_destinations = query_to_map("select destinations.state, count(*) as convicts from convicts inner join destinations on destinations.name = convicts.destination group by destinations.state order by destinations.state")
+    @destinations = Convict.destination_stats
+    @aust_destinations = Convict.aust_destination_stats
+    @state_destinations = Convict.state_destination_stats
   end
 
   def departure_dates
-    @years = query_to_map("select departure_year, count(*) as convicts from convicts where departure_year is not null group by departure_year order by departure_year")
-    @years_map = @years.inject({}) do |result, map| 
-      result[map[:departure_year]] = map[:convicts]
-      result
-    end
-    @months = query_to_map("select departure_month,  count(*) as convicts from convicts where departure_month is not null group by departure_month order by departure_month")
-
-    @decades = {}
-    start_year = 1780
-    until start_year == 1870
-      end_year = start_year + 10
-      @decades["#{start_year}-#{end_year}"] = ActiveRecord::Base.connection.execute("select count(*) as convicts from convicts where departure_year >= #{start_year} and departure_year < #{end_year}").first['convicts']
-      start_year = end_year
-    end
+    @years, @years_map = Convict.year_stats
+    @months = Convict.month_stats
+    @decades = Convict.decade_stats
   end
 
   def court_counties
-    @counties = query_to_map("select court_county, count(*) as convicts from convicts where court_county is not null group by court_county order by court_county")
-    @top_counties = query_to_map('select court_county, count(*) as convicts from convicts where court_county is not null group by court_county order by count(*) desc limit 15')
-  end
-
-private
-
-  def query_to_map(query)
-    ActiveRecord::Base.connection.execute(query).collect { |m| m.with_indifferent_access }
+    @counties = Convict.court_county_stats
+    @top_counties = Convict.top_county_stats
   end
 
 end
--- app/models/convict.rb
+++ app/models/convict.rb
@@ -1,3 +1,65 @@
 class Convict < ActiveRecord::Base
 
+  def self.boat_stats
+    query_to_map("select boat, count(*) as convicts, count(distinct(departure_date)) as voyage_count from convicts where boat is not null group by boat order by boat")
+  end
+
+  def self.top_boat_stats
+    query_to_map("select boat, count(*) as convicts from convicts group by boat order by count(*) desc limit 100")
+  end
+
+  def self.destination_stats
+    query_to_map("select destination, destinations.state, count(*) as convicts from convicts left join destinations on destinations.name = convicts.destination where destination  is not null group by destination order by destination")
+  end
+
+  def self.aust_destination_stats
+    self.destination_stats.inject({}) do |result, value|
+      destination = Destination.find_by_name(value[:destination])
+      result["#{destination.current_name}"] = value[:convicts] unless destination.blank?
+      result
+    end
+  end
+
+  def self.state_destination_stats
+    query_to_map("select destinations.state, count(*) as convicts from convicts inner join destinations on destinations.name = convicts.destination group by destinations.state order by destinations.state")
+  end
+
+  def self.year_stats
+    year_stats = query_to_map("select departure_year, count(*) as convicts from convicts where departure_year is not null group by departure_year order by departure_year")
+    year_stats_map = year_stats.inject({}) do |result, map| 
+      result[map[:departure_year]] = map[:convicts]
+      result
+    end
+    return year_stats, year_stats_map
+  end
+
+  def self.month_stats
+    query_to_map("select departure_month,  count(*) as convicts from convicts where departure_month is not null group by departure_month order by departure_month")
+  end
+
+  def self.decade_stats
+    decades = {}
+    start_year = 1780
+    until start_year == 1870
+      end_year = start_year + 10
+      decades["#{start_year}-#{end_year}"] = ActiveRecord::Base.connection.execute("select count(*) as convicts from convicts where departure_year >= #{start_year} and departure_year < #{end_year}").first['convicts']
+      start_year = end_year
+    end
+    return decades
+  end
+
+  def self.court_county_stats
+    query_to_map("select court_county, count(*) as convicts from convicts where court_county is not null group by court_county order by court_county")
+  end
+
+  def self.top_county_stats
+    query_to_map('select court_county, count(*) as convicts from convicts where court_county is not null group by court_county order by count(*) desc limit 15')
+  end
+
+private
+
+  def self.query_to_map(query)
+    ActiveRecord::Base.connection.execute(query).collect { |m| m.with_indifferent_access }
+  end
+
 end
