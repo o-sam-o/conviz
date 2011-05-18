8e7cc4afb445d72e978e8fd145f9983db5cf5be4
Sam Cavenagh <cavenaghweb@hotmail.com>
2011-05-18 22:09:25 +1000

    add charts and graphs to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def index
-
+    @years, @years_map = Convict.year_stats
+    @top_boats = Convict.top_boat_stats
+    @aust_destinations = Convict.aust_destination_stats
   end
 
   def about
