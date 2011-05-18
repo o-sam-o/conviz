ff22e4f735aad19795319f70bc1a37da499ad017
Sam Cavenagh <cavenaghweb@hotmail.com>
2011-05-18 22:24:04 +1000

    add convicts locations to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
     @years, @years_map = Convict.year_stats
     @top_boats = Convict.top_boat_stats
     @aust_destinations = Convict.aust_destination_stats
+    @counties = Convict.court_county_stats
   end
 
   def about
