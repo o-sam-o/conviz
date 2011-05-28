892ee5beff7481fc41cc6a5bd63da01251befe08
Sam Cavenagh <cavenaghweb@hotmail.com>
2011-05-28 15:36:03 +1000

    add cache header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_cache_headers
+
+private
+
+  def set_cache_headers
+    response.headers["Cache-Control"] = "public, max-age=3600"
+  end
+
 end
